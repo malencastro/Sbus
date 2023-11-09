4b985d19e0a649457889780b17ad19d17a927400
Mariana Alencastro <mariana.alencastro@valtech.com>
2023-11-09 20:15:19 +0000

    sbus

---
+++ GrpcGreeter/GrpcGreeter/Services/AverageService.cs
@@ -0,0 +1,30 @@
+﻿using Grpc.Core;
+
+namespace GrpcGreeter.Services
+{
+    public class AverageService : AverageCalculator.AverageCalculatorBase
+    {
+        private readonly ILogger<AverageService> _logger;
+        public AverageService(ILogger<AverageService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override async Task<AverageResponse> CalculateAverage(IAsyncStreamReader<AverageRequest> requestStream, ServerCallContext context)
+        {
+            var sum = 0;
+            var count = 0.0;
+            while (await requestStream.MoveNext())
+            {
+                sum += requestStream.Current.InputNumber;
+                count++;
+            }
+
+            var average = sum / count;
+            return new AverageResponse
+            {
+                Average = average
+            };
+        }
+    }
+}
---
+++ GrpcGreeter/GrpcGreeter/Services/SumService.cs
@@ -0,0 +1,22 @@
+﻿using Grpc.Core;
+
+namespace GrpcGreeter.Services
+{
+    public class SumService : Sum.SumBase
+    {
+        private readonly ILogger<SumService> _logger;
+
+        public SumService(ILogger<SumService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<SumResponse> AddTwoNumbers(SumRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new SumResponse
+            {
+                SumResult = request.NumberOne + request.NumberTwo
+            });
+        }
+    }
+}
